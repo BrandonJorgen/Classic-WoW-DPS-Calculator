d0d9b00348c4c681d9cb32024097fc849e33d554
Unknown <Brandonjorgen@gmail.com>
2019-05-06 00:49:24 -0600

    UI Work

--- Classic Wow DPS Calculator/Assets/Scripts/Formulas.cs
+++ Classic Wow DPS Calculator/Assets/Scripts/Formulas.cs
@@ -6,28 +6,39 @@ using UnityEngine.UI;
 public class Formulas : MonoBehaviour
 {
     public string MinDamage, MaxDamage, WeaponSpeed, MinExtra, MaxExtra, Strength, AttackPower;
-    private float minDamage, maxDamage, weaponSpeed, minExtra, maxExtra, strength, attackPower, result;
-    public bool FlatWeaponDPS, FlatPlusExtraDPS;
+    public GameObject MainHandDPS, OffHandDPS;
+    private float minDamage, maxDamage, weaponSpeed, minExtra, maxExtra, strength, attackPower, resultMainHand, resultOffHand;
+    public bool FlatPlusExtraDPS;
 
+    //TODO Enchantments
+    //TODO Clear Function
+    
     private void Update()
     {
         //Update Input field values
-        MinDamage = GameObject.Find("MinDamageInput").GetComponent<TMP_InputField>().text;
-        MaxDamage = GameObject.Find("MaxDamageInput").GetComponent<TMP_InputField>().text;
-        WeaponSpeed = GameObject.Find("WeaponSpeedInput").GetComponent<TMP_InputField>().text;
-        Strength = GameObject.Find("StrengthInput").GetComponent<TMP_InputField>().text;
-        AttackPower = GameObject.Find("AttackPowerInput").GetComponent<TMP_InputField>().text;
-        MinExtra = GameObject.Find("MinExtraDamageInput").GetComponent<TMP_InputField>().text;
-        MaxExtra = GameObject.Find("MaxExtraDamageInput").GetComponent<TMP_InputField>().text;
+        MinDamage = GameObject.Find("MainHandMinDamageInput").GetComponent<TMP_InputField>().text;
+        MaxDamage = GameObject.Find("MainHandMaxDamageInput").GetComponent<TMP_InputField>().text;
+        WeaponSpeed = GameObject.Find("MainHandWeaponSpeedInput").GetComponent<TMP_InputField>().text;
+        Strength = GameObject.Find("MainHandStrengthInput").GetComponent<TMP_InputField>().text;
+        AttackPower = GameObject.Find("MainHandAttackPowerInput").GetComponent<TMP_InputField>().text;
+        MinExtra = GameObject.Find("MainHandMinExtraDamageInput").GetComponent<TMP_InputField>().text;
+        MaxExtra = GameObject.Find("MainHandMaxExtraDamageInput").GetComponent<TMP_InputField>().text;
+        MainHandDPS = GameObject.Find("MainHandDPS");
+        OffHandDPS = GameObject.Find("OffHandDPS");
         
         //Update Bool values
-        FlatWeaponDPS = GameObject.Find("FlatWeaponDPSBool").GetComponent<Toggle>().isOn;
-        FlatPlusExtraDPS = GameObject.Find("FlatPlusExtraDamageDPSBool").GetComponent<Toggle>().isOn;
+        FlatPlusExtraDPS = GameObject.Find("MainHandExtraDamageDPSBool").GetComponent<Toggle>().isOn;
+        
+        //Hitting Enter while in an input field Calculates
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Calculate();
+        }
     }
 
     public void Calculate()
     {
-        //Converting strings to int
+        //Converting strings to floats
         float.TryParse(MinDamage, out minDamage);
         float.TryParse(MaxDamage, out maxDamage);
         float.TryParse(WeaponSpeed, out weaponSpeed);
@@ -39,18 +50,29 @@ public class Formulas : MonoBehaviour
         //Converting Strength to Attack Power
         attackPower += (strength * 2);
         
-        //Depending on what bools are checked, clicking this button will do the appropriate formula
-        //
-        if (FlatWeaponDPS)
-        {
-           result = ((minDamage + maxDamage) / 2) / weaponSpeed;
-        }
-
+        //Enchantment Code
+        
+        //Weapon Has Extra Damage
         if (FlatPlusExtraDPS)
         {
-            result = ((minDamage + maxDamage + minExtra + maxExtra) / 2) / weaponSpeed;
+            resultMainHand = (((minDamage + maxDamage + minExtra + maxExtra) / 2) / weaponSpeed) + (attackPower / 14);
+            resultOffHand = ((((minDamage + maxDamage + minExtra + maxExtra) / 2) / weaponSpeed) + (attackPower / 14)) / 2;
         }
+        else
+        {
+            resultMainHand = (((minDamage + maxDamage) / 2) / weaponSpeed) + (attackPower / 14);
+            resultOffHand = ((((minDamage + maxDamage) / 2) / weaponSpeed) + (attackPower / 14)) / 2;
+        }
+
+        MainHandDPS.GetComponent<TextMeshProUGUI>().text = resultMainHand.ToString("#.00") + " DPS";
+        OffHandDPS.GetComponent<TextMeshProUGUI>().text = resultOffHand.ToString("#.00") + " DPS";
         
-        Debug.Log(result);
+        Debug.Log("Main Hand Result: " + resultMainHand);
+        Debug.Log("Off Hand Result: " + resultOffHand);
+    }
+
+    public void ClearAll()
+    {
+        //Sets all UI Text Inputs to 0 or null
     }
 }
